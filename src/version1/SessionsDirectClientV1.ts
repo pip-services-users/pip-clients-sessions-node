9bfee8777757f534fb3812cd425aadff53ad541a
Sergey Seroukhov <seroukhov@gmail.com>
2017-04-24 18:13:54 -0700

    Minor change

--- src/version1/SessionsDirectClientV1.ts
+++ src/version1/SessionsDirectClientV1.ts
@@ -7,7 +7,7 @@ import { DataPage } from 'pip-services-commons-node';
 import { DirectClient } from 'pip-services-net-node';
 
 import { ISessionsClientV1 } from './ISessionsClientV1';
-//import { ISessionsBusinessLogic } from 'pip-services-sessions-node';
+//import { ISessionsController } from 'pip-services-sessions-node';
 import { SessionV1 } from './SessionV1';
 
 export class SessionsDirectClientV1 extends DirectClient<any> implements ISessionsClientV1 {
